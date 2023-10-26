1686f6075f912910a51fda6903722feeeae8bc37
Nayan Bebale <nayanbebale2003@Gmail.com>
2023-10-26 17:25:20 +0530

    Update flight_data.py
    now, we can also call link of flight deals.

--- flight/flight_data.py
+++ flight/flight_data.py
@@ -1,6 +1,6 @@
 class FlightData:
 
-    def __init__(self, price, origin_city, origin_airport, destination_city, destination_airport, out_date, return_date, stop_overs=0, via_city=""):
+    def __init__(self, price, origin_city, origin_airport, destination_city, deep_link, destination_airport, out_date, return_date, stop_overs=0, via_city=""):
         self.price = price
         self.origin_city = origin_city
         self.origin_airport = origin_airport
@@ -8,6 +8,7 @@ class FlightData:
         self.destination_airport = destination_airport
         self.out_date = out_date
         self.return_date = return_date
+        self.deep_link = deep_link
 
         self.stop_overs = stop_overs
         self.via_city = via_city
